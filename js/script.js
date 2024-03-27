cd9702d3a2b5f9b3b3c962a111f7604954bba486
Dunkchump <valik.korolev6@gmail.com>
2024-03-27 19:28:15 +0200

    Created THE FIRST version of project of the studing

--- js/script.js
+++ js/script.js
@@ -1,2 +1,29 @@
 "use strict";
 
+
+const numberOfFilms = +prompt("How many movies had you already wathed?", "");
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {
+
+	},
+	actors: {
+
+	},
+	genres: [
+
+	],
+	privat: false
+    
+};
+
+const question = prompt("Один из просмотренных фильмов?", ""),
+	  questionTwo = prompt("На сколько его оцените?", ""),
+	  questionThre = prompt("Один из просмотренных фильмов?", ""),
+	  questionFour = prompt("На сколько его оцените?", "");
+
+personalMovieDB.movies[question] = questionTwo;
+personalMovieDB.movies[questionThre] = questionFour;
+
+console.log(personalMovieDB);
